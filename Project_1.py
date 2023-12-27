7061a8755dd4127c80585ec905c1c42762e28d10
Raghu Kula <raghuvarankula@gmail.com>
2023-12-27 12:23:57 +0530

    Project file with questions and inferences

---
+++ Project_1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error

440e02946f9b99eb732f5f8d5eb0fb6baa9fc461
Raghu Kula <raghuvarankula@gmail.com>
2023-12-31 21:07:18 +0530

    Project File

--- Project_1.py
+++ Project_1.py
@@ -6,3 +6,7 @@ import os
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
+
+curr_dir = r"U:\Github_Repo\SeattleAirbnbDataset"
+cal_df = pd.read_csv(curr_dir+r"\calendar.csv")
+print(cal_df.describe())
